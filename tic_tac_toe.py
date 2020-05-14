4e44f697741336a4571f7227f131c71360367eaf
Bonizario <gabrielbonizario98@gmail.com>
2020-05-13 21:47:16 -0300

    Decision-making function (1st part)

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -9,3 +9,21 @@ vet_board = np.zeros(dim2)
 empty = 0
 o = 1
 x = -1
+
+
+def get_action(epsilon, dim2, vet_board, empty):
+    """Returns the action to be taken by the agent"""
+    r = np.random.rand()
+
+    possible_actions = []
+    for i in range(dim2):
+        if vet_board(i) == empty:
+            possible_actions.append(i)
+
+    if r <= epsilon:
+        n = len(possible_actions)
+        index = np.random.choice(n)
+        action = possible_actions[index]
+        return action
+    # else:
+        # To do...
