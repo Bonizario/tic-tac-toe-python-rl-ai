01a0aa0ce72a2b2ae62368cf225141da8237e344
Bonizario <gabrielbonizario98@gmail.com>
2020-05-27 21:19:27 -0300

    Game over and draw board functions

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,7 +6,7 @@ def main():
     dim = 3
     dim2 = dim * dim
 
-    vet_board = np.zeros(dim2)
+    # vet_board = np.zeros(dim2)
 
     empty = 0
     o = 1
@@ -16,13 +16,59 @@ def main():
     # initializing the Q-table
     Q = np.zeros((3**dim2, dim2))
 
+    # episodes parameters
+    N_episodes = 30000
+    alpha = 0.5  # learning rate
+    gamma = 0.95  # discount rate
+    min_epsilon = 0  # 0% probability to take random actions
+    max_epsilon = 1  # 100% probability to take random actions
+    decay_rate = 0.001  # if too high, epsilon will approach 0 too fast
+    epsilon = 1
+
     for episode in range(N_episodes):
-        Q = play_one_episode(Q)
-        # epsilon =
-        # print()
+        Q = play_one_episode(Q, o, x, epsilon, dim2, empty, alpha, gamma)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * \
+            np.exp(-decay_rate * episode)
+        if episode % 1000 == 0:
+            print(epsilon)
+
+    # play_teste()
 
 
-def play_one_episode(Q, o, x, epsilon, dim2, vet_board, empty):
+def play_teste(Q, dim2, empty, x, o):
+    vet_board = np.zeros(dim2)
+    game_over = False
+    p1 = o
+    p2 = x
+    current_player = []
+    state = get_state(dim2, vet_board, empty, o)
+
+    while not game_over:
+        # switch players
+        current_player = p2 if current_player == p1 else p1
+
+        # current_player makes a move
+        if current_player == p1:
+            # robot uses only his experiences (epsilon == 0)
+            action = get_action(Q, state, 0, dim2, vet_board, empty)
+        else:
+            action = int(input('Faça sua jogada: '))
+
+        # filling the board
+        vet_board[action] = current_player
+
+        # checking if the match is over
+        game_over, winner = game_over()
+
+        # changing to a new state
+        new_state = get_state(dim2, vet_board, empty, o)
+
+        # atualizing state, Q-table will be stored in a external for loop
+        state = new_state
+
+
+def play_one_episode(Q, o, x, epsilon, dim2, empty, alpha, gamma):
+    vet_board = np.zeros(dim2)
     game_over = False
     p1 = o
     p2 = x
@@ -36,9 +82,11 @@ def play_one_episode(Q, o, x, epsilon, dim2, vet_board, empty):
 
         # current_player makes a move
         if current_player == p1:
-            action = take_action(epsilon, dim2, vet_board, empty) # apprentice robot
+            # apprentice robot
+            action = get_action(Q, state, epsilon, dim2, vet_board, empty)
         else:
-            action = take_action(1, dim2, vet_board, empty) # always random moves
+            # always random moves
+            action = get_action(Q, state, 1, dim2, vet_board, empty)
 
         # filling the board
         vet_board[action] = current_player
@@ -56,16 +104,11 @@ def play_one_episode(Q, o, x, epsilon, dim2, vet_board, empty):
         if current_player == p1:
             recorded_state_action_reward.append((state, action, reward))
 
-        # Q-table will be stored in a external for loop
-
-        # atualizing state
+        # atualizing state, Q-table will be stored in a external for loop
         state = new_state
 
-    for s_a_r in reversed(recorded_state_action_reward):
-        Q[state, action] = (1 - alpha)*Q[state, action] + alpha*(reward + gamma*np.max(Q[new_state, :]))
 
-
-def get_action(epsilon, dim2, vet_board, empty):
+def get_action(Q, state, epsilon, dim2, vet_board, empty):
     """Returns the action to be taken by the agent"""
     r = np.random.rand()
 
@@ -79,8 +122,12 @@ def get_action(epsilon, dim2, vet_board, empty):
         index = np.random.choice(n)
         action = possible_actions[index]
         return action
-    # else:
-        # To do...
+    else:
+        Q_vals = Q[state, :]
+        Q_possible = [Q_vals[i] for i in possible_actions]
+        max_Q_possible = np.max(Q_possible)  # not random for 2 or more max
+        actions_max = [i for i in possible_actions if Q_vals == max_Q_possible]
+        action = np.random.choice(actions_max)
 
 
 def get_state(dim2, vet_board, empty, o):
@@ -113,3 +160,56 @@ def get_reward(game_over, winner, current_player):
         return 1
     else:
         return 0
+
+
+def game_over(vet_board, dim, x, o):
+    mat_board = np.reshape(vet_board, (dim, dim))
+
+    for player in (x, o):
+        for i in range(dim):
+            if mat_board[i, :].sum() == player*dim:  # check lines
+                winner = player
+                return True, winner
+            elif mat_board[:, i].sum() == player*dim:  # check columns
+                winner = player
+                return True, winner
+
+    # check diagonals
+    for player in (x, o):
+        if np.sum(np.diag(mat_board)) == player*dim:  # primary
+            winner = player
+            return True, winner
+        elif np.sum(np.diag(np.fliplr(mat_board))) == player*dim:  # secondary
+            winner = player
+            return True, winner
+
+    if np.all((mat_board == 0) is False):  # all fields are not empty
+        winner = None
+        return True, winner
+
+    # game in not over yet
+    winner = None
+    return False, winner
+
+
+def draw_board(vet_board, x, o, dim):
+    mat_board = np.resize(vet_board, (dim, dim))
+
+    print(' ', end='')
+    print('  1', end='  ')
+    print(' 2', end='  ')
+    print(' 3', end='  ')
+    print('')
+    print('-------------')
+
+    for i in range(dim):
+        print(str(i + 1), end=' ')
+        for j in range(dim):
+            print(' ', end='')
+            if mat_board[i, j] == x:
+                print('X  ', end='')
+            elif mat_board[i, j] == o:
+                print('O  ', end='')
+            else:
+                print('-  ', end='')
+        print('')
